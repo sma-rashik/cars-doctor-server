832d45eb013584d365f2621751a026121b5c5d24
Rashik <smarashik00@gmail.com>
2023-05-13 14:08:31 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,140 @@
+const express = require('express')
+const cors = require("cors")
+const jwt = require('jsonwebtoken');
+const app = express()
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+
+
+app.use(cors())
+app.use(express.json())
+
+// https://car-doctor-server-zeta.vercel.app/
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aujohll.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const serviceCollection = client.db('CarsDoctor').collection("Service")
+        const bookingCollection = client.db('CarsDoctor').collection("Bookings")
+        // JWT
+        app.post('/jwt', async (req, res) => {
+
+            const user = req.body
+            console.log(user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            console.log(token);
+            res.send({ token })
+
+        })
+
+
+        // SERVICES
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find();
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+
+            const options = {
+                // sort returned documents in ascending order by title (A->Z)
+                sort: { title: 1 },
+                // Include only the `title` and `imdb` fields in each returned document
+                projection: { title: 1, price: 1, service_id: 1, img: 1 },
+            };
+
+            const result = await serviceCollection.findOne(query, options)
+            res.send(result)
+        })
+
+        // Bookings
+
+        app.get('/bookings', async (req, res) => {
+            console.log(req.query.email)
+            let query = {}
+
+            if (req.query?.email) {
+                query = { email: req.query.email }
+
+            }
+
+            const result = await bookingCollection.find().toArray()
+            res.send(result)
+        })
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            console.log(booking);
+            const result = await bookingCollection.insertOne(booking)
+            res.send(result)
+        })
+        app.get('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+
+            const result = await bookingCollection.findOne(query);
+            console.log(result);
+            if (!result) {
+                res.status(404).send('Booking not found');
+            } else {
+                res.send(result);
+            }
+        });
+
+        app.patch('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updateBooking = req.body;
+            console.log(updateBooking);
+            const updateDoc = {
+                $set: {
+                    status: updateBooking.status
+                }
+            }
+            const result = await bookingCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+
+
+        app.delete('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+
+            const result = await bookingCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Doctor choltese!')
+})
+app.listen(port, () => {
+    console.log(`cars doctor is running on port : ${port}`)
+})
